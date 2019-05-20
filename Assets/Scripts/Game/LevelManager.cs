247534b48d7c4ef07c0b7c37130c321ba4ce4371
tao <996197655@qq.com>
2019-05-20 19:08:15 +0800

    【代码】

---
+++ Assets/Scripts/Game/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager _Ins;
+
+    private void Awake()
+    {
+        _Ins = this;
+    }
+
+    // 开始关卡
+    public void StartGame(int level)
+    {
+
+    }
+
+    
+}
